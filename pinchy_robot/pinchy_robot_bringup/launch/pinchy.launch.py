bfd326714549a0dc4f55a7226363036f6eeabb87
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2025-01-08 17:40:55 -0800

    playing around with xacro and urdf files

--- pinchy_robot/pinchy_robot_bringup/launch/pinchy.launch.py
+++ pinchy_robot/pinchy_robot_bringup/launch/pinchy.launch.py
@@ -36,7 +36,7 @@ def generate_launch_description():
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
     # Load the SDF file from "description" package
-    sdf_file  =  os.path.join(pkg_project_description, 'models', 'pinchy', 'pinchy.urdf')
+    sdf_file  =  os.path.join(pkg_project_description, 'models', 'pinchy', 'pinchy.sdf')
     with open(sdf_file, 'r') as infp:
         robot_desc = infp.read()
 
@@ -47,7 +47,7 @@ def generate_launch_description():
         launch_arguments={'gz_args': PathJoinSubstitution([
             pkg_project_gazebo,
             'worlds',
-            'pinchy.sdf'
+            'pinchy_world.sdf'
         ]), 'on_exit_shutdown': 'true'}.items(),
     )
 
