b3ce771ad88357e121eb972ddcb9c8664691fff3
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2025-01-15 01:17:12 -0800

    working on getting gz ros2 controls to talk

--- pinchy_robot/pinchy_robot_bringup/launch/pinchy_gz.launch.py
+++ pinchy_robot/pinchy_robot_bringup/launch/pinchy_gz.launch.py
@@ -14,7 +14,9 @@
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.actions import RegisterEventHandler
 from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
 
@@ -23,40 +25,41 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    
+    # Initialize Arguments
+    gui = LaunchConfiguration("gui", default=False)
+    use_sim_time = LaunchConfiguration("use_sim_time", default=True)
+    use_gazebo = LaunchConfiguration("use_gazebo", default=True)
+
     # Declare arguments
     declared_arguments = []
     declared_arguments.append(
         DeclareLaunchArgument(
             "gui",
-            default_value="false",
+            default_value=gui,
             description="Launch RViz automatically with this launch file.",
         )
     )
-
-    # Initialize Arguments
-    gui = LaunchConfiguration("gui")
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_sim_time",
+            default_value=use_sim_time,
+            description="If true, use simulated clock"),
+    )
+    declared_arguments.append(
+        DeclareLaunchArgument(
+            "use_gazebo",
+            default_value="true",
+            description="Whether to enable gazebo-specific configurations in the robot description.",
+        )
+    )
 
     # gazebo
+    bridge_config_file = PathJoinSubstitution([FindPackageShare("pinchy_robot_bringup"), "config", "pinchy_bridge.yaml"])
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
         ),
-        launch_arguments={"gz_args": " -r -v 3 empty.sdf"}.items(),
-    )
-
-    gz_spawn_entity = Node(
-        package="ros_gz_sim",
-        executable="create",
-        output="screen",
-        arguments=[
-            "-topic",
-            "/robot_description",
-            "-name",
-            "pinchy_system_position",
-            "-allow_remaining",
-            "true",
-        ]
+        launch_arguments={"gz_args": " -r -v 4 empty.sdf"}.items(),
     )
 
     # Get URDF via xacro
@@ -70,21 +73,36 @@ def generate_launch_description():
                     "models",
                     "pinchy.urdf.xacro",
                 ]
-            )
+            ),
+            " ",
+            "use_gazebo:=", use_gazebo
         ]
     )
     robot_description = {"robot_description": robot_description_content}
+    node_robot_state_publisher = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[robot_description],
+    )
+    gz_spawn_entity = Node(
+        package="ros_gz_sim",
+        executable="create",
+        output="screen",
+        arguments=[
+            "-topic", "robot_description",
+            "-name", "pinchy",
+            "-allow_renaming", "true"
+        ]
+    )
 
+    #ros2 control
     robot_controllers = PathJoinSubstitution(
         [
             FindPackageShare("pinchy_robot_bringup"),
             "config",
             "pinchy_controllers.yaml",
         ]
-    )
-
-    rviz_config_file = PathJoinSubstitution(
-        [FindPackageShare("pinchy_robot_bringup"), "config", "pinchy.rviz"]
     )
 
     control_node = Node(
@@ -94,25 +112,28 @@ def generate_launch_description():
         output="both",
     )
 
-    robot_state_pub_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        output="screen",
-        parameters=[robot_description],
-    )
-
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster"],
     )
 
-    robot_controller_spawner = Node(
+    joint_trajectory_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["forward_position_controller", "--param-file", robot_controllers],
+        arguments=["joint_trajectory_controller", "--param-file", robot_controllers],
     )
 
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
+        output='screen'
+    )
+
+    # RViz
+    rviz_config_file = PathJoinSubstitution([FindPackageShare("pinchy_robot_bringup"), "config", "pinchy.rviz"])
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
@@ -124,23 +145,23 @@ def generate_launch_description():
 
     nodes = [
         gazebo,
-        control_node,
-        robot_state_pub_node,
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=gz_spawn_entity,
+                on_exit=[joint_state_broadcaster_spawner],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=joint_state_broadcaster_spawner,
+                on_exit=[joint_trajectory_controller_spawner],
+            )
+        ),
+        bridge,
+        node_robot_state_publisher,
         gz_spawn_entity,
-        joint_state_broadcaster_spawner,
-        robot_controller_spawner,
+        control_node,
         rviz_node,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
-
-    # Bridge ROS topics and Gazebo messages for establishing communication
-    # bridge = Node(
-    #     package='ros_gz_bridge',
-    #     executable='parameter_bridge',
-    #     parameters=[{
-    #         'config_file': os.path.join(pkg_project_bringup, 'config', 'pinchy_bridge.yaml'),
-    #         'qos_overrides./tf_static.publisher.durability': 'transient_local',
-    #     }],
-    #     output='screen'
-    # )
