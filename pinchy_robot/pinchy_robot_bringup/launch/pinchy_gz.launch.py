6e7abd8eaaefa5e0d0e34bcca342aeec4bc897fb
Frans Kouwenhoven <kouwenhoven.frans@gmail.com>
2025-01-15 13:19:26 -0800

    ros2_control working in gazebo. need to get controllers to load now

--- pinchy_robot/pinchy_robot_bringup/launch/pinchy_gz.launch.py
+++ pinchy_robot/pinchy_robot_bringup/launch/pinchy_gz.launch.py
@@ -25,6 +25,9 @@ from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
+    # Important constants
+    robot_xacro_name = "pinchy"
+
     # Initialize Arguments
     gui = LaunchConfiguration("gui", default=False)
     use_sim_time = LaunchConfiguration("use_sim_time", default=True)
@@ -54,12 +57,18 @@ def generate_launch_description():
     )
 
     # gazebo
-    bridge_config_file = PathJoinSubstitution([FindPackageShare("pinchy_robot_bringup"), "config", "pinchy_bridge.yaml"])
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            [FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]
-        ),
-        launch_arguments={"gz_args": " -r -v 4 empty.sdf"}.items(),
+        PythonLaunchDescriptionSource([FindPackageShare("ros_gz_sim"), "/launch/gz_sim.launch.py"]),
+        launch_arguments={"gz_args": " -r -v -v4 empty.sdf", "on_exit_shutdown": "true"}.items(),
+    )
+
+    bridge_params = PathJoinSubstitution([FindPackageShare("pinchy_robot_bringup"), "config", "pinchy_bridge.yaml"])
+    node_gazebo_ros_bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        parameters=[{
+            'config_file': bridge_params
+        }]
     )
 
     # Get URDF via xacro
@@ -79,7 +88,7 @@ def generate_launch_description():
         ]
     )
     robot_description = {"robot_description": robot_description_content}
-    node_robot_state_publisher = Node(
+    robot_state_publisher = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         output="screen",
@@ -91,7 +100,7 @@ def generate_launch_description():
         output="screen",
         arguments=[
             "-topic", "robot_description",
-            "-name", "pinchy",
+            "-name", robot_xacro_name,
             "-allow_renaming", "true"
         ]
     )
@@ -105,7 +114,7 @@ def generate_launch_description():
         ]
     )
 
-    control_node = Node(
+    ros2_control_ = Node(
         package="controller_manager",
         executable="ros2_control_node",
         parameters=[robot_controllers],
@@ -115,21 +124,13 @@ def generate_launch_description():
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_state_broadcaster"],
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
     )
 
     joint_trajectory_controller_spawner = Node(
         package="controller_manager",
         executable="spawner",
-        arguments=["joint_trajectory_controller", "--param-file", robot_controllers],
-    )
-
-    # Bridge
-    bridge = Node(
-        package='ros_gz_bridge',
-        executable='parameter_bridge',
-        arguments=['/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock'],
-        output='screen'
+        arguments=["joint_trajectory_controller", "--controller-manager", "/controller_manager", "--param-file", robot_controllers],
     )
 
     # RViz
@@ -144,10 +145,20 @@ def generate_launch_description():
     )
 
     nodes = [
+        robot_state_publisher,
         gazebo,
+        gz_spawn_entity,
+        node_gazebo_ros_bridge,
+        rviz_node,
         RegisterEventHandler(
             event_handler=OnProcessExit(
-                target_action=gz_spawn_entity,
+                target_action=robot_state_publisher,
+                on_exit=[ros2_control_],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=ros2_control_,
                 on_exit=[joint_state_broadcaster_spawner],
             )
         ),
@@ -157,11 +168,6 @@ def generate_launch_description():
                 on_exit=[joint_trajectory_controller_spawner],
             )
         ),
-        bridge,
-        node_robot_state_publisher,
-        gz_spawn_entity,
-        control_node,
-        rviz_node,
     ]
 
     return LaunchDescription(declared_arguments + nodes)
